f84206521103d6027703044f4c5a479ff03b8815
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-08 12:45:24 +0900

    fix: correct paths for job-related environment variables

--- jobs/profile.zsh
+++ jobs/profile.zsh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 export ROOT=${HOME}/schedios
 export DATA=${ROOT}/data
-export JOBS=${DATA}/jobs
-export LIBS=${DATA}/libs
-export TASKS=${DATA}/tasks
+export JOBS=${ROOT}/jobs
+export LIBS=${ROOT}/libs
+export TASKS=${ROOT}/tasks
