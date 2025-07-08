10619106401ec316a674dc7b19ab6d97edc9a288
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-08 13:33:23 +0900

    feat: add hmmscan function for Pfam domain scanning

---
+++ libs/hmmscan.zsh
@@ -0,0 +1,58 @@
+#!/usr/bin/env zsh
+
+function hmmscan_pfam_domain() {
+    # This function runs hmmscan on a given task directory to scan for Pfam domains.
+    # dependencies: hmmer
+
+    # === Defaults ===
+    local threads=10
+    local cutoffs="ga,nc,tc"
+
+    # === Usage ===
+    if (( $# < 1 )); then
+        echo "Usage: scan_pfam_domain <taskname> [--threads <threads>] [--cutoffs <comma_separated_list_of_cutoffs>]" >&2
+        exit 1
+    fi
+
+    # === Arguments ===
+    local taskname="$1"
+    shift
+
+    while (( $# > 0 )); do
+        case "$1" in
+            --threads|-t)
+                threads="$2"
+                shift 2
+                ;;
+            --cutoffs|-c)
+                cutoffs="$2"
+                shift 2
+                ;;
+            *)
+                echo "[Error] Unknown option: $1" >&2
+                exit 1
+                ;;
+        esac
+    done
+
+    # === Set paths ===
+    local taskdir="${TASKS}/${taskname}"
+    local fasta="${taskdir}/input/all_species.fasta"
+    local hmm="${DATA}/Pfam/Pfam-A.hmm"
+
+    # === Make directories ===
+    local outdir="${taskdir}/output_$(date +"%Y-%m-%d-%H-%M-%S")"
+    mkdir -p "$outdir"
+
+    # === HMMSCAN Execution ===
+    IFS=',' read -r -a cutoff_arr <<< "$cutoffs"
+    for cut in "${cutoff_arr[@]}"; do
+        hmmscan \
+            -o "${outdir}/output/hmmscan_${cut}.txt" \
+            --domtblout "${taskdir}/output/domtblout_${cut}.txt" \
+            --cpu "$threads" \
+            --cut_${cut} \
+            "$hmm" \
+            "$fasta"
+    done
+}
