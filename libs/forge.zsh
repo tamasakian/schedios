357f4c054effad58311ac9ca23a6d41693eb922a
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-24 19:46:01 +0900

    feat: add MCScanX input preparation and execution workflow

--- libs/forge.zsh
+++ libs/forge.zsh
@@ -100,3 +100,88 @@ function forge_fastoma_input(){
     echo "[FINISHED] FastOMA input prepared in: ${taskdir}"
 }
 
+function forge_mcscanx() {
+    # This function prepares input for MCScanX by creating a task directory
+    # and generating pairwise comparisons of species.
+    # Dependencies: bithon, fasp, diamond, MCScanX
+
+    # === Defaults ===
+    local threads=4
+    if [[ "$1" == "--threads" ]]; then
+        threads="$2"
+        shift 2
+    fi
+
+    # === Usage ===
+    if (( $# < 1 )); then
+        echo "Usage: forge_mcscanx_input [--threads <threads> <sp_name1> <sp_name2> ..." >&2
+        exit 1
+    fi
+
+    # === Arguments ===
+    local sp_names=("${@}")
+    local threads=4
+    local taskdir="${TASKS}/mcscanx_$(date +"%Y-%m-%d-%H-%M-%S")"
+    mkdir -p "${taskdir}/tmp" "${taskdir}/fasta" "${taskdir}/fasta_prefix" "${taskdir}/gff" "${taskdir}/bed" "${taskdir}/bed_prefix" "${taskdir}/input" "${taskdir}/output"
+
+    echo "[INFO] Preparing MCScanX input for species: ${sp_names[*]}"
+    if [[ ${#sp_names[@]} -lt 2 ]]; then
+        echo "[Error] At least two species are required for MCScanX." >&2
+        exit 1
+    fi
+
+    # === Prepare FASTA, GFF, and BED files for each species ===
+    for sp in "${sp_names[@]}"; do
+        echo "[INFO] Processing species: ${sp}"
+        local sp_fs="${sp// /_}"
+        local gn="${sp_fs%%_*}"
+
+        local pep="${DATA}/${gn}/${sp_fs}.pep.all.fasta"
+        local cds="${DATA}/${gn}/${sp_fs}.cds.all.fasta"
+        local gff="${DATA}/${gn}/${sp_fs}.genome.gff"
+
+        mkdir -p "${taskdir}/tmp/${sp_fs}"
+        cp "$pep" "${taskdir}/tmp/${sp_fs}/pep.fasta"
+        cp "$cds" "${taskdir}/tmp/${sp_fs}/cds.fasta"
+        cp "$gff" "${taskdir}/gff/${sp_fs}.gff"
+        bithon gls -i "${taskdir}/tmp/${sp_fs}" -o "${taskdir}/tmp/${sp_fs}"
+        cp "${taskdir}/tmp/${sp_fs}/longest.pep.fasta" "${taskdir}/fasta/${sp_fs}.fasta"
+        rm -r "${taskdir}/tmp/${sp_fs}"
+
+        python3 -m g2bp fasta4mcscanx "${taskdir}/gff/${sp_fs}.gff" "${taskdir}/bed/${sp_fs}.bed" "${taskdir}/fasta/${sp_fs}.fasta"
+        python3 -m fasp prefix_to_sequence_ids "${taskdir}/fasta/${sp_fs}.fasta" "${taskdir}/fasta_prefix/${sp_fs}.fasta" "$sp_fs"
+        awk -v sp="${sp_fs}_" 'BEGIN{OFS=FS="\t"} {$2=sp $2; print}' "${taskdir}/bed/${sp_fs}.bed" > "${taskdir}/bed_prefix/${sp_fs}.bed"
+
+    done
+
+    # === Create pairs and run MCScanX ===
+    echo "[INFO] Creating pairs and running MCScanX"
+
+    for ((i=1; i<=${#sp_names[@]}-1; i++)); do
+        for ((j=i+1; j<=${#sp_names[@]}; j++)); do
+            local sp1="${sp_names[i]}"
+            local sp2="${sp_names[j]}"
+            local sp1_fs="${sp1// /_}"
+            local sp2_fs="${sp2// /_}"
+            local gn1="${sp1_fs%%_*}"
+            local gn2="${sp2_fs%%_*}"
+            local pairdir="${taskdir}/input/${sp1_fs}__${sp2_fs}"
+            mkdir -p "$pairdir"
+
+            echo "[INFO] Processing pair: ${sp1} vs ${sp2}"
+            cp "${taskdir}/fasta_prefix/${sp1_fs}.fasta" "${pairdir}/${sp1_fs}.fasta"
+            cp "${taskdir}/fasta_prefix/${sp2_fs}.fasta" "${pairdir}/${sp2_fs}.fasta"
+            cat "${taskdir}/bed_prefix/${sp1_fs}.bed" "${taskdir}/bed_prefix/${sp2_fs}.bed" > "${pairdir}/${sp1_fs}__${sp2_fs}.gff"
+            diamond makedb --in "${pairdir}/${sp2_fs}.fasta" -d "${pairdir}/${sp2_fs}.dmnd"
+            diamond blastp \
+                -q "${pairdir}/${sp1_fs}.fasta" \
+                -d "${pairdir}/${sp2_fs}.dmnd" \
+                -o "${pairdir}/${sp1_fs}__${sp2_fs}.blast" \
+                --threads "$threads" \
+                --outfmt 6
+            MCScanX "${pairdir}/${sp1_fs}__${sp2_fs}"
+            cp "${pairdir}/${sp1_fs}__${sp2_fs}.collinearity" "${taskdir}/output/${gn1}__${gn2}.collinearity"
+        done
+    done
+}
+
