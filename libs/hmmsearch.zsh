526f1a0aa1ae8d2ba12d148ade60d5511e835558
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-29 23:21:13 +0900

    feat: add input preparation function for hmmsearch

--- libs/hmmsearch.zsh
+++ libs/hmmsearch.zsh
@@ -1,5 +1,45 @@
 #!/usr/bin/env zsh
 
+function forge_hmmsearch_input() {
+    # This function prepares input for HMMsearch by creating a task directory
+    # and generating a combined FASTA file from multiple species.
+    # Dependencies: bithon, fasp
+
+    # === Arguments ===
+    if (( $# < 2 )); then
+        echo "Usage: forge_hmmsearch_input <sp_name1> <sp_name2> ..." >&2
+        exit 1
+    fi
+    names=("${@}")
+
+    # === Make directories ===
+    taskdir="${TASKS}/hmmsearch_$(date +"%Y-%m-%d-%H-%M-%S")"
+    mkdir -p "${taskdir}/fasta" "${taskdir}/input"
+
+    for name in "${names[@]}"; do
+        sp_name="${name// /_}"
+        pep="${DATA}/${sp_name}/${sp_name}.pep.all.fasta"
+        cds="${DATA}/${sp_name}/${sp_name}.cds.all.fasta"
+        tmpdir="${taskdir}/tmp_${sp_name}"
+        mkdir -p "$tmpdir"
+        cp "$pep" "${tmpdir}/pep.fasta"
+        cp "$cds" "${tmpdir}/cds.fasta"
+
+        # === Longest isoforms selection with bithon ===
+        bithon gls -i "$tmpdir" -o "$tmpdir"
+
+        python3 -m fasp prefix_to_sequence_ids \
+            "${tmpdir}/longest.pep.fasta" \
+            "${taskdir}/fasta/${sp_name}.fasta" \
+            "$sp_name"
+
+        rm -r "$tmpdir"
+
+    done
+
+    cat "${taskdir}/fasta/"*.fasta > "${taskdir}/input/all_species.fasta"
+}
+
 function hmmsearch_pfam_domain() {
     # This function runs hmmsearch on a given task directory to search for Pfam domains.
     # dependencies: hmmer
