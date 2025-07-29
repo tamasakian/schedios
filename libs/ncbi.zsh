e9fb9f2b7be88af61ab07f236bb790798f81166e
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-29 21:08:21 +0900

    refactor: simplify genome fetch script and improve output organization

--- libs/ncbi.zsh
+++ libs/ncbi.zsh
@@ -2,42 +2,32 @@
 
 function fetch_genome_taxid() {
     # This script downloads genomes by Taxonomy ID from NCBI using the NCBI Datasets CLI.
-    # Dependencies: datasets, dataformat
-    if (( $# != 1 )); then
-        echo "Usage: fetch_genome_taxid <Taxonomy ID>" >&2
-        exit 1
-    fi
+    # Dependencies: datasets, dataformat, jq
 
     # === Arguments ===
+    if (( $# != 1 )); then
+        echo "Usage: fetch_genome_taxid <taxid>" >&2
+        exit 1
+    fi
     taxid="$1"
 
-    # === Obtain species and genus names from Taxonomy ID ===
+    # === Obtain species name from Taxonomy ID ===
     json=$(datasets summary taxonomy taxon "$taxid")
-    name=$(echo "$json" | jq -r '.reports[0].taxonomy.current_scientific_name.name')
+    name=$(echo "$json" | jq -r '.reports[0].taxonomy.classification.species.name')
     if [[ -z "$name" || "$name" == "null" ]]; then
         echo "[ERROR] Failed to retrieve scientific name for taxid: $taxid" >&2
         exit 1
     fi
-    gn_name="${name%% *}"
     sp_name="${name// /_}"
 
-    echo "[INFO] Taxonomy ID: $taxid"
-    echo "[INFO] Scientific name: $name"
-    echo "[INFO] Genus name: $gn_name"
-    echo "[INFO] Species name: $sp_name"
-
-    # === Make directories ===
-    dir="${DATA}/${gn_name}"
+    # === Download genome data ===
+    dir="${DATA}/${sp_name}"
     mkdir -p "$dir"
-
-    # === Download ===
     datasets download genome taxon "$taxid" \
         --include genome,protein,cds,gff3 \
         --reference \
         --annotated \
         --filename "${dir}/${sp_name}.zip"
-
-    unzip -o "${dir}/${sp_name}.zip" -d "$dir"
 
     # === Parse assembly data report ===
     dataformat tsv genome \
@@ -45,16 +35,17 @@ function fetch_genome_taxid() {
         --fields organism-name,assminfo-name,accession \
         | tail -n +2 | IFS=$'\t' read -r org asm acc
 
-    echo "[INFO] Assembly: $asm"
-    echo "[INFO] Accession: $acc"
-
+    # === Process downloaded files ===
+    unzip -o "${dir}/${sp_name}.zip" -d "$dir"
     datadir="${dir}/ncbi_dataset/data/${acc}"
-    mv "${datadir}/${acc}_${asm}_genomic.fna" "${dir}/${sp_name}.dna.toplevel.fasta"
-    mv "${datadir}/cds_from_genomic.fna"      "${dir}/${sp_name}.cds.all.fasta"
-    mv "${datadir}/protein.faa"               "${dir}/${sp_name}.pep.all.fasta"
-    mv "${datadir}/genomic.gff"               "${dir}/${sp_name}.genome.gff"
+    cp "${datadir}/${acc}_${asm}_genomic.fna" "${dir}/${sp_name}.dna.toplevel.fasta"
+    cp "${datadir}/cds_from_genomic.fna"      "${dir}/${sp_name}.cds.all.fasta"
+    cp "${datadir}/protein.faa"               "${dir}/${sp_name}.pep.all.fasta"
+    cp "${datadir}/genomic.gff"               "${dir}/${sp_name}.genome.gff"
 
-    # === Cleanup ===
-    rm -rf "${dir}/ncbi_dataset"
+    # === Cleanup garbage ===
+    rm -r "${dir}/ncbi_dataset"
     rm "${dir}/${sp_name}.zip"
+    rm "${dir}/README.md"
+    rm "${dir}/md5sum.txt"
 }
