83b4101ca9f4d08fd284a54811b7e1a8d5665c74
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-07-30 14:20:14 +0900

    feat: add input preparation function for FastOMA

--- libs/fastoma.zsh
+++ libs/fastoma.zsh
@@ -1,5 +1,49 @@
 #!/usr/bin/env zsh
 
+function input4fastoma(){
+    # This function prepares input for FastOMA by selecting longest isoforms and converting sequence IDs.
+    # Dependencies: bithon, biotp, fasp, ete3
+
+    # === Arguments ===
+    if (( $# < 2 )); then
+        echo "Usage: input4fastoma <sp_name1> <sp_name2> ..." >&2
+        exit 1
+    fi
+    names=("${@}")
+
+    # === Make directories ===
+    taskdir="${TASKS}/hmmsearch_$(date +"%Y-%m-%d-%H-%M-%S")"
+    mkdir -p "${taskdir}/proteomes"
+
+    # === Proteomes ===
+    for name in "${names[@]}"; do
+        sp_name="${name// /_}"
+        pep="${DATA}/${sp_name}/${sp_name}.pep.all.fasta"
+        cds="${DATA}/${sp_name}/${sp_name}.cds.all.fasta"
+        tmpdir="${taskdir}/tmp_${sp_name}"
+        mkdir -p "$tmpdir"
+        cp "$pep" "${tmpdir}/pep.fasta"
+        cp "$cds" "${tmpdir}/cds.fasta"
+
+        # === Longest isoforms selection with bithon ===
+        bithon gls -i "$tmpdir" -o "$tmpdir"
+
+        python3 -m fasp prefix_to_sequence_ids \
+            "${tmpdir}/longest.pep.fasta" \
+            "${taskdir}/proteomes/${sp_name}.fasta" \
+            "$sp_name"
+
+        rm -r "$tmpdir"
+
+    done
+
+    # === Species tree ===
+    print -l "${names[@]}" > "${taskdir}/names.txt"
+    taxonkit name2taxid < "${taskdir}/names.txt" | cut -f2 > "${taskdir}/list_taxanomic_id.txt"
+    cat "${taskdir}/list_taxanomic_id.txt" | ete3 ncbiquery --tree > "${taskdir}/sp_tree.nwk"
+    python3 -m biotp clean_tip_label "${taskdir}/sp_tree.nwk" "${taskdir}/species_tree.nwk"
+}
+
 function run_fastoma() {
     # This function runs FastOMA on a given task directory.
     # Dependencies: fastoma
