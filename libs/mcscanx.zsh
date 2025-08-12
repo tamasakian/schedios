4d10226090b994b451984b7ce5098b279a1b239a
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-08-12 14:50:46 +0900

    feat: add MCScanX input preparation and execution workflow

--- libs/mcscanx.zsh
+++ libs/mcscanx.zsh
@@ -1,5 +1,125 @@
 #!/usr/bin/env zsh
 
+function input4mcscanx() {
+    # Prepare input for MCScanX with focus × other comparisons only
+    # Dependencies: bithon, fasp, g2bp, diamond, MCScanX
+
+    # === Arguments ===
+    local threads=4
+    if [[ "$1" == "--threads" ]]; then
+        threads="$2"
+        shift 2
+    fi
+
+    if [[ "$1" != "--focus" ]]; then
+        echo "Usage: input4mcscanx [--threads N] --focus <sp1> ... --all <spA> ..." >&2
+        return 1
+    fi
+    shift
+
+    local focus=()
+    while [[ "$1" != "--all" && $# -gt 0 ]]; do
+        focus+=("$1")
+        shift
+    done
+
+    if [[ "$1" != "--all" ]]; then
+        echo "[Error] Missing --all argument" >&2
+        return 1
+    fi
+    shift
+
+    local all=("$@")
+
+    # === Checks ===
+    if (( ${#focus[@]} < 1 )); then
+        echo "[Error] At least one focus species required" >&2
+        return 1
+    fi
+    if (( ${#all[@]} < 2 )); then
+        echo "[Error] Need at least two total species" >&2
+        return 1
+    fi
+
+    # === Determine other species ===
+    local others=()
+    for sp in "${all[@]}"; do
+        local found=0
+        for fsp in "${focus[@]}"; do
+            [[ "$sp" == "$fsp" ]] && found=1 && break
+        done
+        (( found == 0 )) && others+=("$sp")
+    done
+
+    # === Prepare task directory ===
+    local taskdir="${TASKS}/mcscanx_$(date +"%Y-%m-%d-%H-%M-%S")"
+    mkdir -p "${taskdir}/fasta" "${taskdir}/bed" "${taskdir}/pairs" "${taskdir}/collinearity"
+
+    # === Prepare files for each species ===
+    for sp in "${all[@]}"; do
+        local sp_name="${sp// /_}"
+        local tmpdir="${taskdir}/tmp_${sp_name}"
+        mkdir -p "$tmpdir"
+
+        local pep="${DATA}/${sp_name}/${sp_name}.pep.all.fasta"
+        local cds="${DATA}/${sp_name}/${sp_name}.cds.all.fasta"
+        local gff="${DATA}/${sp_name}/${sp_name}.genome.gff"
+
+        cp "$pep" "${tmpdir}/pep.fasta"
+        cp "$cds" "${tmpdir}/cds.fasta"
+        cp "$gff" "${tmpdir}/genome.gff"
+
+        bithon gls -i "$tmpdir" -o "$tmpdir"
+
+        python3 -m g2bp fasta4mcscanx \
+            "${tmpdir}/genome.gff" \
+            "${tmpdir}/genome.bed" \
+            "${tmpdir}/longest.pep.fasta"
+
+        python3 -m fasp prefix_to_sequence_ids \
+            "${tmpdir}/longest.pep.fasta" \
+            "${taskdir}/fasta/${sp_name}.fasta" \
+            "$sp_name"
+
+        awk -v sp="${sp_name}_" 'BEGIN{OFS=FS="\t"} {$2=sp $2; print}' "${tmpdir}/genome.bed" > "${taskdir}/bed/${sp_name}.bed"
+
+        rm -r "$tmpdir"
+    done
+
+    # === Make diamond DB for all species once ===
+    for sp in "${focus[@]}"; do
+        local sp_name="${sp// /_}"
+        diamond makedb --in "${taskdir}/fasta/${sp_name}.fasta" \
+            -d "${taskdir}/fasta/${sp_name}.dmnd"
+    done
+
+    # === Pairwise comparisons: focus × other ===
+    for sp1 in "${focus[@]}"; do
+        for sp2 in "${others[@]}"; do
+            local sp1_name="${sp1// /_}"
+            local sp2_name="${sp2// /_}"
+            local pairdir="${taskdir}/pairs/${sp1_name}__${sp2_name}"
+            mkdir -p "$pairdir"
+
+            cat "${taskdir}/bed/${sp1_name}.bed" \
+                "${taskdir}/bed/${sp2_name}.bed" \
+                > "${pairdir}/${sp1_name}__${sp2_name}.gff"
+
+            diamond blastp \
+                -q "${taskdir}/fasta/${sp2_name}.fasta" \
+                -d "${taskdir}/fasta/${sp1_name}.dmnd" \
+                -o "${pairdir}/${sp1_name}__${sp2_name}.blast" \
+                --threads "$threads" --outfmt 6
+
+            MCScanX "${pairdir}/${sp1_name}__${sp2_name}"
+
+            cp "${pairdir}/${sp1_name}__${sp2_name}.collinearity" "${taskdir}/collinearity/${sp1_name}__${sp2_name}.collinearity"
+        done
+    done
+}
+
+
+
 function detect_collinearity_within_gene_families() {
     # This function runs downstream analyses for MCScanX collinearity files.
     # It processes each collinearity file in the input directory and generates output files.
